9d2ef92700002189292106a2843e2d9bba4b71cd
Igordl <igor.lucena@ccc.ufcg.edu.br>
2024-10-13 20:34:53 -0300

    Primeira aula restfull - Criando controller 'Hello, world'.

---
+++ src/main/java/br/com/erudio/rest_with_spring_boot_and_java_erudio/Application.java
@@ -0,0 +1,13 @@
+package br.com.erudio.rest_with_spring_boot_and_java_erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
